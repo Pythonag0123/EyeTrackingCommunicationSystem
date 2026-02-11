176238becb5055a0b34695b900839b2b075ee42e
unknown <deepa.ch2604@gmail.com>
2026-02-11 14:34:21 +0530

    frontend: basic layout with camera keyboard output sections

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div style={{padding: "10px", background: "#222", color: "white"}}>
+      <h2>Eye Tracking Communication System</h2>
+    </div>
+  );
+}
+
+export default Header;
---
+++ client/src/components/Keyboard.jsx
@@ -0,0 +1,5 @@
+function Keyboard() {
+  return <div style={{border:"1px solid gray", height:"250px"}}>Virtual Keyboard</div>;
+}
+
+export default Keyboard;
